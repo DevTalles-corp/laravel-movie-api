799ff951df3233bbb47f7bb9101b8c465e74c2ba
Teddy Paz <pazteddy@gmail.com>
2026-07-02 17:03:19 -0400

    feat: implement filter method in GenreRepository for enhanced querying capabilities

--- app/Repositories/Contracts/GenreRepositoryInterface.php
+++ app/Repositories/Contracts/GenreRepositoryInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface GenreRepositoryInterface extends BaseRepositoryInterface
 {
-    //
+   public function filter(
+    array $filters,
+    string $sortBy = 'name',
+    string $order = 'asc'
+   ):Collection;
 }
--- app/Repositories/Eloquent/GenreRepository.php
+++ app/Repositories/Eloquent/GenreRepository.php
@@ -4,14 +4,34 @@ namespace App\Repositories\Eloquent;
 
 use App\Models\Genre;
 use App\Repositories\Contracts\GenreRepositoryInterface;
+use \Illuminate\Database\Eloquent\Collection;
 
 class GenreRepository extends BaseRepository implements GenreRepositoryInterface
 {
+    private const SORTABLE = ['name','create_at','is_active'];
+
     /**
      * Create a new class instance.
      */
     public function __construct(Genre $model)
     {
         parent::__construct($model);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function filter(array $filters, string $sortBy = 'name', string $order = 'asc'): Collection {
+        $sortBy = in_array($sortBy, self::SORTABLE) ? $sortBy : 'name';
+        $order = strtolower($order) === 'desc'? 'desc':'asc';
+        return Genre::query()
+        ->when(isset($filters['search']),
+            fn($q) => $q->where('name','ilike',"%{$filters['search']}%")
+        )
+         ->when(isset($filters['is_active']),
+            fn($q) => $q->where('is_active',filter_var($filters['is_active'], FILTER_VALIDATE_BOOLEAN))
+        )
+        ->orderBy($sortBy,$order)
+        ->get();
     }
 }
