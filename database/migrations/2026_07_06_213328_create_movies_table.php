40c34463404a15c354d5a991be6b316b57f04619
Teddy Paz <pazteddy@gmail.com>
2026-07-06 23:53:48 -0400

    feat: add migrations for movies and genre_movie tables

---
+++ database/migrations/2026_07_06_213328_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('synopsis')->nullable();
+            $table->smallInteger('year');
+            $table->decimal('rating',3,1);
+            $table->string('director')->nullable();
+            $table->integer('duration')->nullable()->comment('duración en minutos');
+            $table->string('poster')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
