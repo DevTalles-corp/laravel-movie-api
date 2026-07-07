a3bd857e860305aeb1f9d20bbb1fdeb06c8cb199
Teddy Paz <pazteddy@gmail.com>
2026-07-07 16:58:34 -0400

    feat: add MovieSeeder to populate movie data in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         $this->call(GenreSeeder::class);
+        $this->call(MovieSeeder::class);
     }
 }
