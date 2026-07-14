1c9aac31ae4f3d23e9f6fc5e2ebbdd54134def00
Teddy Paz <pazteddy@gmail.com>
2026-07-14 09:44:39 -0400

    feat: enhance user registration with error handling for token generation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use App\Traits\ApiResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -16,11 +17,19 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $user = User::create($request->validated());
-        $token = Auth::guard('api')->login($user);
-
-        return $this->successResponse($this->tokenPayload($user,$token),
+        try {
+            $token = Auth::guard('api')->login($user);
+            if(!$token)
+              {
+                return $this->errorResponse('Error al generar el token.', 500);
+              }
+            return $this->successResponse($this->tokenPayload($user,$token),
                                       "Usuario registrado exitosamente.",
                                       201);
+        } catch (JWTException $e) {
+            return $this->errorResponse('Error al generar el token.', 500);
+        }
+        
     }
 
     public function tokenPayload(User $user, string $token):array
@@ -31,5 +40,25 @@ class AuthController extends Controller
             'expires_in' => Auth::guard('api')->factory()->getTTL() * 60,
             'user' => new UserResource($user)
         ];
+    }
+    public function login(Request $request):JsonResponse
+    {
+        $credentials = $request->validate(
+            [
+                'email' => ['required','email'],
+                'password' => ['required','string'],
+            ]
+        );
+        if(! $token = Auth::guard('api')->attempt($credentials))
+          {
+            return $this->errorResponse('Credenciales incorrectas.', 401);
+          }
+        return $this->successResponse(
+            $this->tokenPayload(Auth::guard('api')->user(),$token)
+        );
+    }
+    public function me()
+    {
+        return $this->successResponse(new UserResource(Auth::guard('api')->user()));
     }
 }
