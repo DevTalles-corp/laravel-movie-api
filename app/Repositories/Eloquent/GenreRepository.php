ba5c9d1597e99833ee8db11a5035291a02d20e5a
Teddy Paz <pazteddy@gmail.com>
2026-07-22 15:26:52 -0400

    refactor: improve code formatting and consistency in GenreRepository using cache

--- app/Repositories/Eloquent/GenreRepository.php
+++ app/Repositories/Eloquent/GenreRepository.php
@@ -4,12 +4,13 @@ namespace App\Repositories\Eloquent;
 
 use App\Models\Genre;
 use App\Repositories\Contracts\GenreRepositoryInterface;
-use \Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use \Illuminate\Database\Eloquent\Collection;
 
 class GenreRepository extends BaseRepository implements GenreRepositoryInterface
 {
-    private const SORTABLE = ['name','create_at','is_active'];
+    private const SORTABLE = ['name', 'create_at', 'is_active'];
 
     /**
      * Create a new class instance.
@@ -22,33 +23,42 @@ class GenreRepository extends BaseRepository implements GenreRepositoryInterface
     /**
      * @inheritDoc
      */
-    public function filter(array $filters, string $sortBy = 'name', string $order = 'asc'): Collection {
+    public function filter(array $filters, string $sortBy = 'name', string $order = 'asc'): Collection
+    {
         $sortBy = in_array($sortBy, self::SORTABLE) ? $sortBy : 'name';
-        $order = strtolower($order) === 'desc'? 'desc':'asc';
-        return Genre::query()
-        ->when(isset($filters['search']),
-            fn($q) => $q->whereRaw('LOWER(name) LIKE ?',['%'.strtolower($filters['search']).'%'])
-        )
-         ->when(isset($filters['is_active']),
-            fn($q) => $q->where('is_active',filter_var($filters['is_active'], FILTER_VALIDATE_BOOLEAN))
-        )
-        ->orderBy($sortBy,$order)
-        ->get();
+        $order = strtolower($order) === 'desc' ? 'desc' : 'asc';
+        $key = 'genres.index.'.md5(serialize(compact('filters', 'sortBy', 'order')));
+
+        $genres = Cache::remember($key, now()->addMinutes(30), function () use ($filters, $sortBy, $order) {
+            return Genre::query()
+                ->when(isset($filters['search']),
+                    fn ($q) => $q->whereRaw('LOWER(name) LIKE ?', ['%'.strtolower($filters['search']).'%']))
+                ->when(isset($filters['is_active']),
+                    fn ($q) => $q->where('is_active', filter_var($filters['is_active'], FILTER_VALIDATE_BOOLEAN)))
+                ->orderBy($sortBy, $order)
+                ->get()
+                ->toArray();
+        });
+
+        return Genre::hydrate($genres);
     }
 
     /**
      * @inheritDoc
      */
-    public function findBySlugOrFail(string $slug): Model {
-        return Genre::where('slug',$slug)->firstOrFail();
+    public function findBySlugOrFail(string $slug): Model
+    {
+        return Genre::where('slug', $slug)->firstOrFail();
     }
 
     /**
      * @inheritDoc
      */
-    public function restore(int $id): Model {
+    public function restore(int $id): Model
+    {
         $genre = Genre::withTrashed()->findOrFail($id);
         $genre->restore();
+
         return $genre->refresh();
     }
 }
