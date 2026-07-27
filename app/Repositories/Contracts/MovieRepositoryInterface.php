5fc7251dc10e9fe82eabc5381badf0814a5d1515
Teddy Paz <pazteddy@gmail.com>
2026-07-27 14:08:41 -0400

    feat: update MovieRepositoryInterface to include pagination in filter method

--- app/Repositories/Contracts/MovieRepositoryInterface.php
+++ app/Repositories/Contracts/MovieRepositoryInterface.php
@@ -3,15 +3,16 @@
 namespace App\Repositories\Contracts;
 
 use App\Models\Movie;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface MovieRepositoryInterface extends BaseRepositoryInterface
 {
     public function filter(
-    array $filters,
-    string $sortBy = 'title',
-    string $order = 'asc'
-   ):Collection;
+        array $filters,
+        string $sortBy = 'title',
+        string $order = 'asc',
+        int $perPage = 15,
+    ): LengthAwarePaginator;
 
-   public function syncGenres(Movie $movie, array $genreIds):void;
+    public function syncGenres(Movie $movie, array $genreIds): void;
 }
