c4fffe5ce41664f0ad99b2d84eedea88c63d1041
Teddy Paz <pazteddy@gmail.com>
2026-07-27 16:45:20 -0400

    feat: enhance exception handling in Application configuration for API responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Middleware\CheckRole;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,5 +25,23 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
+        // 404
+        $exceptions->render(function (ModelNotFoundException|NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Recurso no encontrado.'], 404);
+            }
+        });
+        // 401
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'No autenticado.'], 401);
+            }
+        });
+        // 500
+        $exceptions->renderable(function (\Throwable $e, Request $request) {
+            if ($request->is('api/*') && ! config('app.debug')) {
+                return response()->json(['message' => 'Error interno del servidor.'], 500);
+            }
+        });
     })
     ->create();
